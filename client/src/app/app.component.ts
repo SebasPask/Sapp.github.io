e3f2ce8c3e205a8a68de1093cfde2207c35d14c2
SebasPask <sebaspask@gmail.com>
2017-06-19 21:56:09 +1000

    19/6
    Update

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { User } from './models/user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  public title = 'PASKFY';
+  public user: User;
+  public identity;
+  public token;
+
+  constructor(){
+   this.user = new User('','','','','','ROLE_USER','');
+  }
+  public onSubmit(){
+    console.log(this.user);
+  }
+
+}
