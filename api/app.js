c17efab1d7a3a8472e7ef99020216dcde3d7e19a
SebasPask <sebaspask@gmail.com>
2017-06-22 20:15:39 +1000

    22/6
    Login done

--- api/app.js
+++ api/app.js
@@ -10,7 +10,7 @@ var song_routes = require('./routes/song');
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 // configurar cabeceras hide.bs.tooltip
-app.use((req, res, nest) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 import { User } from './models/user';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   public title = 'PASKFY';
   public user: User;
   public identity;
   public token;
+  public errorMessage;
 
-  constructor(){
+  constructor(
+    private _userService:UserService
+    ){
    this.user = new User('','','','','','ROLE_USER','');
+  }
+  ngOnInit(){
+   
   }
   public onSubmit(){
     console.log(this.user);
+    this._userService.singup(this.user).subscribe(
+      response => {
+        console.log(response);
+      },
+      error => {
+        var errorMessage = <any>error;
+        if(errorMessage != null){
+          this.errorMessage = error;
+          console.log(error);
+        }
+      }
+    );
   }
 
 }
